6badf2a08511486f132805628251d1c4de299352
s3xmaxman <badside3919@gmail.com>
2025-03-10 19:28:11 +0900

    Refactor: Remove unused getURL function and redirectTo prop from AuthModal
    The getURL function was removed from the AuthModal component as it was no longer needed. The redirectTo prop was also removed, as the redirect logic is now handled elsewhere.
    
    This change simplifies the AuthModal component and removes unnecessary code.

--- components/Modals/AuthModal.tsx
+++ components/Modals/AuthModal.tsx
@@ -30,16 +30,6 @@ const AuthModal = () => {
     }
   };
 
-  const getURL = () => {
-    let url = process.env.NEXT_PUBLIC_VERCEL_URL ?? "http://localhost:3000/";
-
-    // Make sure to include `https://` when not localhost.
-    url = url.startsWith("http") ? url : `https://${url}`;
-    // Make sure to include a trailing `/`.
-    url = url.endsWith("/") ? url : `${url}/`;
-    return url;
-  };
-
   return (
     <Modal
       title="おかえりなさい"
@@ -52,7 +42,6 @@ const AuthModal = () => {
         magicLink
         providers={["google"]}
         supabaseClient={supabaseClient}
-        redirectTo={getURL()}
         appearance={{
           theme: ThemeSupa,
           variables: {
